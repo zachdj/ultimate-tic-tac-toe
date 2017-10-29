c0b58774d50e32ce271f9abedd0193c8a499d01c
Zach Jones <zachydj@gmail.com>
2017-10-29 01:07:41 -0400

    Sets up logical screen size and mouse event scaling

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import scenes
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0))
+LOGICAL_WIDTH = 1920
+LOGICAL_HEIGHT = 1080
+display = pygame.Surface((LOGICAL_WIDTH, LOGICAL_HEIGHT))  # we will draw on this surface then transform it to screen coordinates
 clock = pygame.time.Clock()
 
-active_scene = scenes.MainMenu(screen) # TODO
+active_scene = scenes.MainMenu(screen)  # TODO
 
 while active_scene != None:
     pressed_keys = pygame.key.get_pressed()
@@ -23,6 +26,11 @@ while active_scene != None:
                 quit_attempt = True
             elif event.key == pygame.K_F4 and alt_pressed:
                 quit_attempt = True
+        elif event.type in [pygame.MOUSEBUTTONUP, pygame.MOUSEBUTTONDOWN, pygame.MOUSEMOTION]:
+            # transform from actual screen coordinates to logical coordinates
+            scaling_factor_x = LOGICAL_WIDTH / screen.get_width()
+            scaling_factor_y = LOGICAL_HEIGHT / screen.get_height()
+            event.pos = (event.pos[0]*scaling_factor_x, event.pos[1]*scaling_factor_y)
 
         if quit_attempt:
             active_scene.terminate()
@@ -31,7 +39,9 @@ while active_scene != None:
 
     active_scene.process_input(filtered_events, pressed_keys)
     active_scene.update()
-    active_scene.render(screen)
+    active_scene.render(display)
+
+    pygame.transform.scale(display, screen.get_size(), screen)
 
     active_scene = active_scene.next
 
